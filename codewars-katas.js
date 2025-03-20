11fc5bac01b40ee3d3cf5d6aab1a4192e013578b
Riciere Marcone <riciere@outlook.com.br>
2025-03-20 18:06:18 -0300

    Maximum Subarray sum - JS

--- codewars-katas.js
+++ codewars-katas.js
@@ -1,3 +1,29 @@
+// Maximum Subarray sum
+
+// The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+
+// maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])
+// // should be 6: [4, -1, 2, 1]
+// Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+
+// Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+function maxSequence(arr) {
+  let maxSum = 0;      // Maior soma encontrada
+  let currentSum = 0;  // Soma atual do subarray
+  
+  for (let i = 0; i < arr.length; i++) {
+    // Atualiza currentSum, comparando se devemos continuar a soma ou recomeçar com arr[i]
+    currentSum = Math.max(arr[i], currentSum + arr[i]);
+    
+    // Atualiza maxSum se a soma atual for maior
+    maxSum = Math.max(maxSum, currentSum);
+  }
+
+  return maxSum;
+}
+
+
 // Sum of Digits / Digital Root
 
 // Digital root is the recursive sum of all the digits in a number.
