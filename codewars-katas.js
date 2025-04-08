3347900eb17a7d48c8430dfe1b01446608969306
Riciere Marcone <riciere@outlook.com.br>
2025-04-07 22:40:50 -0300

    Roman Numerals Decoder

--- codewars-katas.js
+++ codewars-katas.js
@@ -1,3 +1,53 @@
+// Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+// Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+// Example:
+// "MM"      -> 2000
+// "MDCLXVI" -> 1666
+// "M"       -> 1000
+// "CD"      ->  400
+// "XC"      ->   90
+// "XL"      ->   40
+// "I"       ->    1
+// Help:
+// Symbol    Value
+// I          1
+// V          5
+// X          10
+// L          50
+// C          100
+// D          500
+// M          1,000
+// Courtesy of rosettacode.org
+
+function solution(roman) {
+  const symbol = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  };
+
+  let value = 0;
+  let previous = 0;
+
+  for (let i = 0; i < roman.length; i++) {
+      const current = symbol[roman[i]];
+      if (previous < current) {
+          value += current - 2 * previous;
+      } else {
+          value += current;
+      }
+      previous = current;
+  }
+
+  return value;
+}
+
 // Maximum Subarray sum
 
 // The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
