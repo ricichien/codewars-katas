150bc826e5c32dd550bd93c82250328457ffec91
Riciere Marcone <riciere@outlook.com.br>
2026-07-31 22:45:36 -0300

    brick by brick a wall is being build

--- review.ts
+++ review.ts
@@ -45,3 +45,70 @@ function sumEven(numbers: number[]): number {
 
   return sum;
 }
+
+// Reforcando
+// function countVowels2(string: string) {
+//   let counter = 0;
+//   for (let i = 0; string.length > i; i++) {
+//     if (
+//       string[i] === "a" ||
+//       string[i] === "e" ||
+//       string[i] === "i" ||
+//       string[i] === "o" ||
+//       string[i] === "u"
+//     ) {
+//       counter += 1;
+//     }
+//   }
+//   return counter;
+// }
+
+// function countEven2(number: number[]) {
+//   let counter = 0;
+//   for (let i = 0; number.length > i; i++) {
+//     if (number[i] % 2 === 0) {
+//       counter += 1;
+//     }
+//   }
+//   return counter;
+// }
+
+// function countGreaterThanTen2(number: number[]) {
+//   let counter = 0;
+//   for (let i = 0; number.length > i; i++) {
+//     if (number[i] > 10) {
+//       counter += 1;
+//     }
+//   }
+//   return counter;
+// }
+
+// function sumEven2(number: number[]) {
+//   let sum = 0
+//   for (let i = 0; number.length > i; i++) {
+//     if (number[i] % 2 === 0) {
+//       sum += number[i];
+//     }
+//   }
+//   return sum;
+// }
+
+function findMax(number: number[]) {
+  let maxNumber = number[0];
+  for (let i = 0; number.length > i; i++) {
+    if (number[i] > maxNumber) {
+      maxNumber = number[i];
+    }
+  }
+  return maxNumber;
+}
+
+function findMin(number: number[]) {
+  let minNumber = number[0];
+  for (let i = 0; number.length > i; i++) {
+    if (number[i] < minNumber) {
+      minNumber = number[i];
+    }
+  }
+  return minNumber;
+}
